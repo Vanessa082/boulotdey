07b5a77320e7e0089acf872fbd5a289666ae3c46
vanessa082 <wahvanessa22@gmail.com>
2024-09-23 11:46:30 +0100

    added a color palete for green color and all shades

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
