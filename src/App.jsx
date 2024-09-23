05146f0ee9cc245cfe77be7b232d2492c5194c19
vanessa082 <wahvanessa22@gmail.com>
2024-09-23 13:46:32 +0100

    created a landing page and import it in the app.jsc as a route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import {Route, Routes } from "react-router-dom"
 import AppWrapper from "./component/common/app-wrapper"
+import LandingPage from "./pages/landing-page/landing-page"
 
 function App() {
 
   return (
     <Routes>
       <Route element={<AppWrapper/>}>
-      <Route index element={} />
+      <Route index element={<LandingPage />} />
       </Route>
     </Routes>
   )
--- src/component/common/app-wrapper.jsx
+++ src/component/common/app-wrapper.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
+import NavbarOne from "./header/navbar-one";
 
 export default function AppWrapper() {
   return(
     <>
-
+    <NavbarOne />
     <Outlet/>
-    
+
     </>
   )
 }
